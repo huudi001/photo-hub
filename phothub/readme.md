fbc278791810218f5cb410ef8211507001d4b830
khalid hassan <abuuanas30@hotmail.com>
2017-11-19 22:05:41 +0300

    refactoring

---
+++ phothub/readme.md
@@ -0,0 +1,23 @@
+#  title
+
+* photo-hub
+
+
+#  Author
+*  Khalid Hashi
+
+
+#  pre-requisites
+
+* python
+* a strong internet connection
+* the link to the deployed site
+
+# Known bugs
+
+* search view function has a flaw
+
+
+# contributers
+
+* one  
--- phothub/views.py
+++ phothub/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 import datetime as dt
 from .models import Post,tags,User
 
-def trending_pics(request):    
+def trending_pics(request):
     date = dt.date.today()
     posts = Post.todays_posts()
     all_posts = Post.display_post()
     return render(request, 'all_pics/trending_pics.html', {"date": date,"posts":posts,"all_posts": all_posts })
 def search_results(request):
 
-    if 'post' in request.GET and request.GET["post"]:
-        search_term = request.GET.get("post")
+    if 'tag' in request.GET and request.GET["tag"]:
+        search_term = request.GET.get("tag")
         searched_posts = Post.search_by_tag(search_term)
         message = f"{search_term}"
 
